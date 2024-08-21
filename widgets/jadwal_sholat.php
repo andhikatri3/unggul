3e63efbc9ee3f1cc9d2c7dd30c4e6fbc704b0289
andhikatri3 <andhikatri3@yahoo.com>
2024-08-21 09:58:15 +0700

    update API jadwal sholat

--- widgets/jadwal_sholat.php
+++ widgets/jadwal_sholat.php
@@ -6,33 +6,35 @@
 	padding: 2.5rem 1rem !important; 
 }
 </style>
+<?php
+	$tgl 	= date('Y-m-d');
+	$tgl_besok = mktime(0,0,0,date("n"),date("j")+1,date("Y"));
+	$besok 	= date("Y-m-d", $tgl_besok);
+	$kota 	= config_item('kode_kota');
+	$kode 	= json_decode(file_get_contents('https://api.myquran.com/v2/sholat/kota/'.$kota), true);
+	$nama 	= $kode['data']['lokasi'];
+	$waktu 	= json_decode(file_get_contents('https://api.myquran.com/v2/sholat/jadwal/'.$kota.'/'.$tgl), true);
 
-	<script>
-		const KODE_KOTA = "<?= config_item('kode_kota') ?>";
-		const TANGGAL = "<?= date('Y-m-d') ?>";
-		const BESOK = "<?= date("Y-m-d", mktime(0,0,0,date("n"),date("j")+1,date("Y"))) ?>";
-	</script>
-	<script src="<?= base_url("$this->theme_folder/$this->theme/assets/js/widget.min.js") ?>"></script>
-
+?>
 	<div class="w-100 mb-50">
 		<div class="row align-items-center">
 			<div class="col-md-12 col-sm-12 col-lg-12">
 				<div class="time-title w-100">
 					<h4 class="mb-0">Jadwal Sholat & Imsak</h4>
-					<p class="mb-0 thm-clr">
-						<span data-name="tanggal"></span> di 
-						<span data-name="kota"></span>
+					<p class="mb-0 thm-clr"> 
+						<?= $waktu['data']['jadwal']['tanggal'] ?> di <?= $nama ?>
 					</p>
 				</div>
 			</div>
 			<div class="col-md-12 col-sm-12 col-lg-12">
 				<ul class="time-list2 d-flex flex-wrap w-100 mb-0 list-unstyled">
-					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Imsak </span> <h5 data-name="imsak" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
-					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Subuh</span><h5 data-name="subuh" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
-					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Dhuhur</span><h5 data-name="dzuhur" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
-					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Ashar</span><h5 data-name="ashar" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
-					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Maghrib</span><h5 data-name="maghrib" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
-					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Isya'</span><h5 data-name="isya" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
+					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Imsak </span> <h5 class="mb-0"><?= $waktu['data']['jadwal']['imsak'] ?></h5> </li>
+					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Subuh </span><h5 class="mb-0"><?= $waktu['data']['jadwal']['subuh'] ?></h5> </li>
+					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Dhuhur </span><h5 class="mb-0"><?= $waktu['data']['jadwal']['dzuhur'] ?></h5> </li>
+					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Ashar </span><h5 class="mb-0"><?= $waktu['data']['jadwal']['ashar'] ?></h5> </li>
+					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Maghrib </span><h5 class="mb-0"><?= $waktu['data']['jadwal']['maghrib'] ?></h5> </li>
+					<li id="jadwal-shalat" class="jadwalsholat text-center"><span>Isya' </span><h5 class="mb-0"><?= $waktu['data']['jadwal']['isya'] ?></h5> </li>
+
 				</ul>
 			</div>
 		</div>
--- widgets/jadwal_sholat_top.php
+++ widgets/jadwal_sholat_top.php
@@ -1,11 +1,13 @@
 <?php if (config_item('kode_kota')): ?>
-	<script>
-		const KODE_KOTA = "<?= config_item('kode_kota') ?>";
-		const TANGGAL = "<?= date('Y-m-d') ?>";
-		const BESOK = "<?= date("Y-m-d", mktime(0,0,0,date("n"),date("j")+1,date("Y"))) ?>";
-	</script>
-	<script src="<?= base_url("$this->theme_folder/$this->theme/assets/js/widget.min.js") ?>"></script>
-
+<?php
+	$tgl 	= date('Y-m-d');
+	$tgl_besok = mktime(0,0,0,date("n"),date("j")+1,date("Y"));
+	$besok 	= date("Y-m-d", $tgl_besok);
+	$kota 	= config_item('kode_kota');
+	$kode 	= json_decode(file_get_contents('https://api.myquran.com/v2/sholat/kota/'.$kota), true);
+	$nama 	= $kode['data']['lokasi'];
+	$waktu 	= json_decode(file_get_contents('https://api.myquran.com/v2/sholat/jadwal/'.$kota.'/'.$tgl), true);
+?>
 	<section>
 		<div class="w-100 position-relative">
 			<div class="time-wrap2 w-100">
@@ -14,19 +16,18 @@
 						<div class="time-title w-100">
 							<h4 class="mb-0">Jadwal Sholat & Imsak</h4>
 							<p class="mb-0 thm-clr">
-								<span data-name="tanggal"></span> di 
-								<span data-name="kota"></span>
+								<?= $waktu['data']['jadwal']['tanggal'] ?> di <?= $nama ?>
 							</p>
 						</div>
 					</div>
 					<div class="col-md-12 col-sm-12 col-lg-8">
 						<ul class="time-list2 d-flex flex-wrap w-100 mb-0 list-unstyled text-center">
-							<li id="jadwal-shalat"><span>Imsak </span> <h5 data-name="imsak" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
-							<li id="jadwal-shalat"><span>Subuh </span><h5 data-name="subuh" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
-							<li id="jadwal-shalat"><span>Dhuhur </span><h5 data-name="dzuhur" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
-							<li id="jadwal-shalat"><span>Ashar </span><h5 data-name="ashar" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
-							<li id="jadwal-shalat"><span>Maghrib </span><h5 data-name="maghrib" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
-							<li id="jadwal-shalat"><span>Isya' </span><h5 data-name="isya" class="mb-0"><i class="fa fa-spinner fa-pulse"></i></h5> </li>
+							<li id="jadwal-shalat"><span>Imsak </span> <h5 class="mb-0"><?= $waktu['data']['jadwal']['imsak'] ?></h5> </li>
+							<li id="jadwal-shalat"><span>Subuh </span><h5 class="mb-0"><?= $waktu['data']['jadwal']['subuh'] ?></h5> </li>
+							<li id="jadwal-shalat"><span>Dhuhur </span><h5 class="mb-0"><?= $waktu['data']['jadwal']['dzuhur'] ?></h5> </li>
+							<li id="jadwal-shalat"><span>Ashar </span><h5 class="mb-0"><?= $waktu['data']['jadwal']['ashar'] ?></h5> </li>
+							<li id="jadwal-shalat"><span>Maghrib </span><h5 class="mb-0"><?= $waktu['data']['jadwal']['maghrib'] ?></h5> </li>
+							<li id="jadwal-shalat"><span>Isya' </span><h5 class="mb-0"><?= $waktu['data']['jadwal']['isya'] ?></h5> </li>
 						</ul>
 					</div>
 				</div>
